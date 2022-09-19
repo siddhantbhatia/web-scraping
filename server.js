782fa672177d57f86a7b96f3eb2919ebc3576a6f
Siddhant Bhatia <siddhant8b@gmail.com>
2022-09-20 01:38:08 +0800

    update server with endpoints

--- server.js
+++ server.js
@@ -1,14 +1,43 @@
-var http = require("http");
+const http = require("http");
+const qs = require("querystring");
+const url = require("url");
+const file = require("./functions/file");
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(8081);
+const leaderboardHandlers = require("./functions/leaderboardScrape");
+const chartDataHandlers = require("./functions/chartDataScrape");
+
+http
+  .createServer(async function (req, res) {
+    const { pathname, query } = url.parse(req.url);
+
+    if (req.method !== "GET") {
+      res.end(`{"error": "${http.STATUS_CODES[405]}"}`);
+    } else {
+      if (pathname === "/part1") {
+        res.setHeader("Content-Type", "application/json;charset=utf-8");
+
+        const { date } = qs.parse(query);
+        const fileName = date + ".json";
+
+        const fileData = file.readFile(fileName);
+
+        if (fileData) {
+          res.end(JSON.stringify(fileData));
+        } else {
+          const leaderboardData =
+            await leaderboardHandlers.scrapeLeaderboardData();
+
+         //  const chartData = await chartDataHandlers.fetchChartData(date);
+          const output = { leaderboardData };
+
+          file.writeFile(fileName, output);
+
+          res.end(JSON.stringify(output));
+        }
+      }
+    }
+  })
+  .listen(8081);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log("Server running at http://127.0.0.1:8081/");
